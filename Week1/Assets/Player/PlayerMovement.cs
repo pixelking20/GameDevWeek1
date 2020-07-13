288f5096c3221b3c4bfa45eb2acce1cfbdfc2655
Matthew Ramsey <36775051+pixelking20@users.noreply.github.com>
2020-07-13 14:57:28 -0400

    testing Forced Text

--- Week1/Assets/Player/PlayerMovement.cs
+++ Week1/Assets/Player/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Dear God Please");
+        print("Dear God Why");
     }
 
     // Update is called once per frame
