84f1e6ab13732b1acdc5ec9415d43dc6f1eadbeb
LordofHats <Logan.Crosby09@yahoo.com>
2020-07-13 15:01:13 -0400

    More Trash

--- Week1/Assets/Player/PlayerMovement.cs
+++ Week1/Assets/Player/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello World 2, eletric ...");
+        print("Just Once");
     }
 
     // Update is called once per frame
