b9aa153f777b50df57532210c098734feeb8b152
Matthew Ramsey <36775051+pixelking20@users.noreply.github.com>
2020-07-13 16:02:00 -0400

    player movement

--- Week1/Assets/Player/PlayerMovement.cs
+++ Week1/Assets/Player/PlayerMovement.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public Rigidbody rb;
+    public float speed;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        print("Kill me");
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.AddForce(transform.forward * speed * Input.GetAxis("Vertical"));
+        rb.AddForce(transform.right * speed * Input.GetAxis("Horizontal"));
     }
 }
