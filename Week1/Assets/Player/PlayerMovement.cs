3a0942e55df2bebe42ed0555874c1f06adc8b3d9
Matthew Ramsey <36775051+pixelking20@users.noreply.github.com>
2020-07-13 15:06:37 -0400

    testing new placement of ignore

--- Week1/Assets/Player/PlayerMovement.cs
+++ Week1/Assets/Player/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Just Once");
+        print("Kill me");
     }
 
     // Update is called once per frame
